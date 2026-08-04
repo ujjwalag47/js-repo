769bb3934c0b9226ae6ffb45bb45c775cf9d478b
ujjwalag47 <ujjwalag47@gmail.com>
2026-08-04 15:01:35 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,20 @@
+const accountId=634
+let accountEmail="ujjwal@gmail.com"
+var accountPassword="12345"
+accountCity="Mathura"
 
+let accountState
+
+// accountId=23 // Not Allowed
+//you can use or not use ; not compulsory
+
+accountEmail="ujjwalag@gmail.com"
+accountPassword="685"
+accountCity="Delhi";
+
+console.log(accountId)
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
