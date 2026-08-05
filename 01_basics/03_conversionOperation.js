b7bff5120e55d0f62bdfcfaa7a3bdc3d84dc8000
ujjwalag47 <ujjwalag47@gmail.com>
2026-08-05 04:59:28 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score="33"
+console.log(typeof score)
+console.log(typeof (score))
+
+
+// score=null
+// valueInnumber =Number(score)
+// console.log(typeof valueInnumber) //number
+// console.log(valueInnumber) // 0
+
+// score=undefined
+// valueInnumber =Number(score)
+// console.log(typeof valueInnumber) //number
+// console.log(valueInnumber) // NaN
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+score="Ujjwal"
+valueInnumber =Number(score)
+console.log(typeof valueInnumber) //number
+console.log(valueInnumber) //Nan
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber)
