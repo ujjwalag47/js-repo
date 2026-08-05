de43e6603d1ad96bcaf5ab68de9a815d5b13b88a
ujjwalag47 <ujjwalag47@gmail.com>
2026-08-05 05:18:31 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,15 +1,14 @@
 let score="33"
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-
-// score=null
-// valueInnumber =Number(score)
+score=null
+valueInnumber =Number(score)
 // console.log(typeof valueInnumber) //number
 // console.log(valueInnumber) // 0
 
-// score=undefined
-// valueInnumber =Number(score)
+score=undefined
+valueInnumber =Number(score)
 // console.log(typeof valueInnumber) //number
 // console.log(valueInnumber) // NaN
 
@@ -19,12 +18,12 @@ console.log(typeof (score))
 
 score="Ujjwal"
 valueInnumber =Number(score)
-console.log(typeof valueInnumber) //number
-console.log(valueInnumber) //Nan
+// console.log(typeof valueInnumber) //number
+// console.log(valueInnumber) //Nan
 
 let isLoggedIn = "hitesh"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -32,5 +31,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let somenumber=33
 let stringnumber=String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber)
+
+
+// ****************** Operations *********************
+
+let value=3
+let negvalue= -value
+console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="hello"
+// let str2=" ujjwal"
+// let str3=str1 + str2
+// console.log(str3)
+
+console.log("1"+2) //12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+//Dont write confusing like above,write proper and use()
+// console.log( (3 + 4) * 5 % 3);
+
+console.log(true) //true
+console.log(+true) //1
+//console.log(true+) //error
+console.log(+"") //0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num2)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
