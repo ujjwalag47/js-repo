3785b7b09ae6b13c74e043e3edfff890fe866e67
ujjwalag47 <ujjwalag47@gmail.com>
2026-08-07 17:45:10 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IIFE)
+//Global scope pollute karta hai to hum isliye iife ka use karte hai
+//bahut jaldi call karne ke liye aur baaki code ko bhi farak na pade
+// ()();
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`); //DB CONNECTED
+})(); // ; isliye lagaya jaata hai jis se end ho jaye aur doosra run kar sake
+
+((name) => { //unnamed iife
+    console.log(`db 2 connected ${name}`) //db 2 connected ujjwal
+})("ujjwal")
+
+// How does javascript execute code + call stack video
+//https://www.youtube.com/watch?v=ByhtOgF6uYM&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=25
